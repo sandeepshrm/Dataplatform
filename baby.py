4715a675da27c5034b52ff2746c6d867583e7daa
sandeepshrm <sandeepshrm96@gmail.com>
2022-02-04 17:03:26 +0530

    Update baby.py

--- baby.py
+++ baby.py
@@ -1,6 +1,9 @@
-def addtion():
-  aa = input("Enter values :")
-  sum = 10 + 30
-  print("The addtion of 10 + 30 = ",sum)
+def read_file(filename,sep):
+  with open(filename,'r') as file:
+    data = file.readlines()
+    for d in data:
+      print(d.split(sep)[0],' ',d.split(sep)[1])
+    sum = 10 + 30
+    print("The addtion of 10 + 30 = ",sum)
 
-addtion()
+read_file("testdata.txt",",")
