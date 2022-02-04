701983d764e7f786d43f520ccd8efcfa7a4c6cec
sandeepshrm <sandeepshrm96@gmail.com>
2022-02-04 21:10:15 +0530

    Update baby.py

--- baby.py
+++ baby.py
@@ -2,7 +2,7 @@ def read_file(filename,sep):
   with open(filename,'r') as file:
     data = file.readlines()
     for d in data:
-      print(d.split(sep)[0],' ',d.split(sep)[1])
+      print(d)
     sum = 10 + 30
     print("The addtion of 10 + 30 = ",sum)
 
